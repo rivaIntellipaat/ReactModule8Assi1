7af80e0f9f07b0d3bb4eb79414f4f50dfc92698c
rivaintellipaat <makhani.riva@intellipaat.com>
2024-10-22 18:29:49 +0530

    first commit

---
+++ src/Counter.test.js
@@ -0,0 +1,36 @@
+// src/Counter.test.js
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Counter from './Counter';
+import { act } from 'react';
+
+
+describe('Counter Component', () => {
+    test('initial count value is 0', () => {
+        render(<Counter />);
+        const countElement = screen.getByText(/Counter: 0/i);
+        expect(countElement).toBeInTheDocument();
+    });
+
+    test('increment button functionality', () => {
+        render(<Counter />);
+        const incrementButton = screen.getByText(/Increment/i);
+        fireEvent.click(incrementButton);
+        const countElement = screen.getByText(/Counter: 1/i);
+        expect(countElement).toBeInTheDocument();
+    });
+
+    test('decrement button functionality', () => {
+        render(<Counter />);
+        const decrementButton = screen.getByText(/Decrement/i);
+        const incrementButton = screen.getByText(/Increment/i);
+        
+        // Increment first
+        fireEvent.click(incrementButton);
+        
+        // Then decrement
+        fireEvent.click(decrementButton);
+        const countElement = screen.getByText(/Counter: 0/i);
+        expect(countElement).toBeInTheDocument();
+    });
+});
